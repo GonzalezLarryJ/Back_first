3919180a9a118719cd50e12a09cde6e7cc34a02b
GonzalezLarry <controlalo@outlook.com>
2024-06-01 19:38:03 -0300

    Content to index.js

---
+++ index.js
@@ -0,0 +1,57 @@
+//importacion de modulos necesarios
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+//const cors = require('cors');
+const morgan = require('morgan');
+
+//Importación de rutas
+const example =require('./routes/exampleRoute');
+
+//Inicializar la aplciación
+const app = express();
+
+
+//Configuración del puerto
+const port = process.env.port || 3000;
+
+//MIDDLEWARES
+//Middleware para manejo de cors
+//app.use(cors());
+// Middleware para el manejo de JSON y datos URL encoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// Middleware para el registro de solicitudes HTTP
+app.use(morgan('dev'));
+
+// Conexión a la base de datos
+/* mongoose.connect('mongodb://localhost:27017/mydatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
+.then(() => {
+    console.log('Conexión a la base de datos exitosa');
+})
+.catch(err => {
+    console.error('Error al conectar a la base de datos', err);
+}); */
+
+// Rutas
+//app.use('/api/example', exampleRoutes);
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
+});
+
+// Manejo de errores
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo salió mal!');
+});
+
+// Inicio del servidor
+app.listen(port, () => {
+    console.log(`Servidor en ejecución en el puerto ${port}`);
+});
